99b089b1b1a46cbe7158ff485c468190acfe393f
MikalJohnson <Johnsonmikal24@gmail.com>
2023-07-09 22:27:26 -0700

    Finished Rescue Center

--- forms.py
+++ forms.py
@@ -2,9 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Email, EqualTo
 
-class SignupForm(FlaskForm):
-    fullName = StringField("Full Name", validators=[InputRequired()])
-    email = StringField("Email", validators=[InputRequired(), Email()])
-    password = PasswordField("Password", validators=[InputRequired(), EqualTo('confirmPassword', message="Passwords should match")])
-    confirmPassword = PasswordField("Confirm Password", validators=[InputRequired()])
-    submit = SubmitField("Sign Up")
+class SignUpForm(FlaskForm):
+    full_name = StringField('Full Name')
+    email = StringField('Email',
+                        validators = [InputRequired(), Email()])
+    password = PasswordField('Password', validators = [InputRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators = [InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators = [InputRequired(), Email()])
+    password = PasswordField('Password', validators = [InputRequired()])
+    submit = SubmitField('Login')
+
+class EditPetForm(FlaskForm):
+    name = StringField("Pet's Name", validators = [InputRequired()])
+    age = StringField("Pet's Age", validators = [InputRequired()])
+    bio = StringField("Pet's Bio", validators = [InputRequired()])
+    submit = SubmitField("Edit Pet")
